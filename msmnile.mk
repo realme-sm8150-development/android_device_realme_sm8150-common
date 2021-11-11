78a1461c3ed10b1d01aaeaadb9de2eff9153e75f
slowtrip <edmmyrage@gmail.com>
2021-11-12 00:16:47 +0200

    sm8150-common: Setup namespace

--- msmnile.mk
+++ msmnile.mk
@@ -17,5 +17,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/sm8150-common/sm8150-common-vendor.mk)
