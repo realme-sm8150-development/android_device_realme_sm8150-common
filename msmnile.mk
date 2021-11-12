07e1093c1372fed9a761febe7029918f30c2dbb9
slowtrip <edmmyrage@gmail.com>
2021-11-12 17:50:38 +0200

    sm8150-common: Build OMX stack

--- msmnile.mk
+++ msmnile.mk
@@ -158,6 +158,19 @@ PRODUCT_PACKAGES += \
     SecureElement \
     Tag \
 
+# OMX
+PRODUCT_PACKAGES += \
+    init.qti.media.sh \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
