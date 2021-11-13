514205bae7b3789538b8510b3ca927fdb015e2bc
slowtrip <edmmyrage@gmail.com>
2021-11-13 23:44:26 +0200

    sm8150-common: Copy fstab to ramdisk
    Change-Id: I17033687ee0cc92a84f5d045e85d7df80184233d

--- msmnile.mk
+++ msmnile.mk
@@ -289,6 +289,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
 # Sensors
