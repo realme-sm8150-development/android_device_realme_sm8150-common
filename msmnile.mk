80fe1423f5f66a4997f5ed01d4bdce5a0e731406
slowtrip <edmmyrage@gmail.com>
2021-11-13 18:54:24 +0200

    sm8150-common: Build WFD libs

--- msmnile.mk
+++ msmnile.mk
@@ -241,5 +241,13 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/sm8150-common/sm8150-common-vendor.mk)
